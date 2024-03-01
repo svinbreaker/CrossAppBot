8a2de9f7e15ec0df249be24b6247bf99d0d6d5ce
svinbreaker <veldyaev2013@mail.ru>
2024-03-01 14:00:00 +0400

    added float parser and parsers for nullable primitive types (for commands)

--- СrossAppBot/obj/Debug/net8.0/СrossAppBot.AssemblyInfo.cs
+++ СrossAppBot/obj/Debug/net8.0/СrossAppBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("СrossAppBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45ce22b6eef2c666fbf4a8d1ebe3a5265051990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca4d50eab9e970403e96f931959acf74d25f5c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("СrossAppBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("СrossAppBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
