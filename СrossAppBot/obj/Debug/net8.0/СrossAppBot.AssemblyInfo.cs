33b5dc22105bfc2c31f45e1058f59d7a29185f6d
svinbreaker <veldyaev2013@mail.ru>
2024-07-05 16:58:30 +0400

    Removed unnecessary library from dependencies

--- СrossAppBot/obj/Debug/net8.0/СrossAppBot.AssemblyInfo.cs
+++ СrossAppBot/obj/Debug/net8.0/СrossAppBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("СrossAppBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5078afd33b0bc6b51c3a26199c24254a2dbe3ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcda3eb0ef52cff517356701e89fcef7175e1c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("СrossAppBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("СrossAppBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
